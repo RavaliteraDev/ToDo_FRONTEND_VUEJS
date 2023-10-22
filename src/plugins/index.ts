358f29d5adf8804fa5ebda1e88cea7480859b5b5
RavaliteraDev <ravaliterarijavola.dev@gmail.com>
2023-10-22 10:07:49 +0300

    Add VueRouter and Pinia : SUCCESS

--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -5,11 +5,15 @@
  */
 
 // Plugins
-import vuetify from './vuetify'
+import { pinia } from './pinia'
+import { router } from './vue-router'
+import { vuetify } from './vuetify'
 
 // Types
 import type { App } from 'vue'
 
 export function registerPlugins (app: App) {
+  app.use(pinia)
+  app.use(router)
   app.use(vuetify)
 }
